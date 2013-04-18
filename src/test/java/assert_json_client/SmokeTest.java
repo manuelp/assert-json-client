4044475cf1d261551d56de4792734ea1069fb65e
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-04-18 17:52:56 +0200

    More significant test case

--- src/test/java/assert_json_client/SmokeTest.java
+++ src/test/java/assert_json_client/SmokeTest.java
@@ -11,10 +11,17 @@ import static assert_json.java.AssertJson.assertJson;
 public class SmokeTest {
   @Test
   public void shouldAssertJsonProperties() {
-    assertJson("{\"ciao\":1,\"list\":[1,\"something\"]}", new HashMap<String, Object>() {
+    assertJson("{\"ciao\":1," +
+        "\"list\":[1,\"something\"]," +
+        "\"other\":{\"sub\":[42]}}", new HashMap<String, Object>() {
       {
         put("ciao", 1);
         put("list", Arrays.asList(1, "something"));
+        put("other", new HashMap<String, Object>() {
+          {
+            put("sub", Arrays.asList(42));
+          }
+        });
       }
     });
   }
